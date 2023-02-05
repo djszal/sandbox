bc7fdcfb540b1bf97fe91cd1fef3d6af49c35c44
djszal <szalank.dj@gmail.com>
2023-02-05 09:24:37 -0500

    first push

---
+++ test.js
@@ -0,0 +1,19 @@
+// let output = document.querySelector(".output");
+// output.innerHTML = "";
+
+let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
+
+while (i >= 0) {
+  if (i === 10) {
+    console.log("CountDown 10");
+  } else if (i === 0) {
+    console.log("Blast off!!");
+  } else {
+    console.log(`${i}`);
+  }
+  i--;
+}
